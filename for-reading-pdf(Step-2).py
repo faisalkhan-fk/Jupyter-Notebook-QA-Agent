29a5f086c8f2aba41d9480ca3bd44a9e834f00d6
Faisal Khan <faisalkhan7863030@gmail.com>
2026-09-19 01:22:42 +0530

    Delete for-reading-pdf(Step-2).py

--- for-reading-pdf(Step-2).py
+++
@@ -1,7 +0,0 @@
-import pymupdf                                           # for reading PDFs
-from sentence_transformers import SentenceTransformer   # to convert text into numeric vectors
-import faiss                                             # for fast similarity search
-import numpy as np
-import google.generativeai as genai                     # for generating answers
-from dotenv import load_dotenv                          # to load the API key from .env
-import os
