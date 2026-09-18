fabc656b8000f63ff1777c98fdd999e159d5bac9
Faisal Khan <faisalkhan7863030@gmail.com>
2026-09-19 01:24:22 +0530

    Delete Api-key(Step-8).py

--- Api-key(Step-8).py
+++
@@ -1,6 +0,0 @@
-load_dotenv()   # load the .env file
-
-api_key = os.getenv("GEMINI_API_KEY")   # use the exact variable name from your .env file
-genai.configure(api_key=api_key)
-
-gen_model = genai.GenerativeModel("gemini-3.6-flash")
